381a93d6c01a6afecfd7d2052f79f279df67dbe1
jullia2005 <100949271+jullia2005@users.noreply.github.com>
2022-03-15 15:25:10 -0300

    Update cadastro.php

---
+++ Inicio - testes/cadastro.php
@@ -0,0 +1,11 @@
+<?php
+    echo "nome:" .$_POST['nome'];
+    echo "Sobrenome:" .$_POST['sobrenome'];
+    echo "<br>";
+
+    if (isset($_POST['testando']))
+    {
+        echo $_POST['blabla'];
+        echo "<br>";
+    }
+    ?>
